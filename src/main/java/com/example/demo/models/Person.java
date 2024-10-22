60e6f2b733fce8c6bc6ac4fdf3223e512c61ebea
Kiaryy <martin360.moron@gmail.com>
2024-10-21 23:28:47 -0300

    HOTFIX: added getters and setters to person entity

--- src/main/java/com/example/demo/models/Person.java
+++ src/main/java/com/example/demo/models/Person.java
@@ -1,22 +1,28 @@
 package com.example.demo.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorColumn;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "Discriminator")
-
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 
 @Table(name = "Person")
 public class Person extends Base{
+    @Column(name = "name")
     protected String name;
+    @Column(name = "phone_number")
     protected Long phoneNumber;
 }
