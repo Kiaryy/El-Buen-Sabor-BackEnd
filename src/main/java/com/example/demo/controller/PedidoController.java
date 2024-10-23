70bf851433ff3ef07f0604d8e02a94051db44d79
Nacho2070 <ignaciomrtn90@gmail.com>
2024-10-22 21:58:26 -0300

    log4j

--- src/main/java/com/example/demo/controller/PedidoController.java
+++ src/main/java/com/example/demo/controller/PedidoController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import lombok.extern.log4j.Log4j2;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
