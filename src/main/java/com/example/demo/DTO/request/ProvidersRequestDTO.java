7538b177a67b56a5745f371d49592177682c32fc
Kiaryy <martin360.moron@gmail.com>
2024-11-05 12:18:41 -0300

    FIX: changed product to enum instad of String

--- src/main/java/com/example/demo/DTO/request/ProvidersRequestDTO.java
+++ src/main/java/com/example/demo/DTO/request/ProvidersRequestDTO.java
@@ -2,10 +2,12 @@ package com.example.demo.DTO.request;
 
 import java.util.List;
 
+import com.example.demo.models.enums.ArticleCategory;
+
 
 public record ProvidersRequestDTO(
         String name,
-        String product,
+        ArticleCategory product,
         Double shippingCost,
         Long phoneNumber,
         List<Long> articles
